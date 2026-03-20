0ffac2cc862a58cfff65b12ff9365855b4847b69
Grayframer <bruceamcknight@gmail.com>
2026-03-19 22:57:05 -0400

    Revert "added another line to Shipping_Cost_Calculator.py"
    This reverts commit 8387bf75d023a7d7c6c060ca879e20bcbf3a4a7b.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,7 +1,6 @@
 # Shipping Cost Calculator
 
 # Here is a new update by Grayframer.
-# Here is another update by Grayframer.
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
